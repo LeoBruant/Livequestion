ae90345a603f0af73f4c67e35cfb359ce1c79e24
Maxence <61044105+maxmlk@users.noreply.github.com>
2020-03-02 15:10:15 +0100

    Add files via upload

--- page_inscription/index.php
+++ page_inscription/index.php
@@ -1,5 +1,35 @@
 <?php
-	require_once('../header.php');
+	//require_once('../header.php');
+?>
+
+<?php
+$host='localhost';
+$db = 'piscinelivequestion';
+$username = 'root';
+$password = 'root';
+
+ 
+//require_once 'dbconfig.php';
+ 
+$dsn= "mysql:host=$host;dbname=$db";
+ 
+try{
+ // create a PDO connection with the configuration data
+ $conn = new PDO($dsn, $username, $password);
+ 
+ /* display a message if connected to database successfully
+ if($conn){
+ echo "Connected to the <strong>$db</strong> database successfully!"; 
+        }*/
+
+$sql = "INSERT INTO utilisateur (pseudo_utilisateur, mdp_utilisateur, genre_utilisateur, email_utilisateur) VALUES ($_POST['nom'], $_POST['mot_de_passe'],$_POST['genre'],$_POST['email']";
+
+}
+catch (PDOException $e){
+ // report error message
+ echo $e->getMessage();
+}
+
 ?>
 
 
@@ -15,15 +45,22 @@
 		<form method="post">
 		    <div>
 		        <label for="nom">Nom d'utilisateur</label>
-		        <input type="text" id="nom" name="nom_utilisateur">
+		        <input type="text" name="nom">
 		    </div>
 		    <div>
-		        <label for="prenom">mot de passe :</label>
-		        <input type="text" id="prenom" name="prenom_utilisateur">
+		        <label for="mot_de_passe">mot de passe :</label>
+		        <input type="text" name="mot_de_passe">
+		    </div>
+		    <div>
+				<label for="genre"> genre : </label>
+		    	<select name="genre">
+				    <option name="homme">homme</option>
+				    <option name="femme">femme</option>
+				</select>
 		    </div>
 		    <div>
 		        <label for="email">e-mail :</label>
-		        <input type="email" id="mail" name="email_utilisateur">
+		        <input type="email" name="email">
 		    </div>
 		    <div class="button">
 				  <button type="submit">Finaliser l'inscription</button>
