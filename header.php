a208fb07d41266c31828d7c79d476f034539e817
Léo Bruant <61012951+LeoBruant@users.noreply.github.com>
2020-03-10 19:15:40 +0100

    Update header.php
    mise à jour du header

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="css/header.css">
 <nav class="nav bg-light">
     <p class="title">Livequestion</p>
-    <p class="questions"><a href="index.php">Les questions</a></p>
+    <p class="questions"><a href="les_questions.php">Les questions</a></p>
     <p class="ask_question"><a href="ask_question.php">Poser une question</a></p>
 </nav>
