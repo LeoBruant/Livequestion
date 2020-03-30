ab2487caa34d3f445de025ddac5e4974792ade9c
Maxence <61044105+maxmlk@users.noreply.github.com>
2020-03-30 14:57:15 +0200

    Update connection_utilisateur.php

--- page_inscription/connection_utilisateur.php
+++ page_inscription/connection_utilisateur.php
@@ -3,7 +3,7 @@
 <head>
 	<title>Login</title>
 	<?php require_once('../header.php'); ?>
-	<?php require_once('php my admin'); ?>
+	<?php require_once('connection_bdd.php'); ?>
 </head>
 <body>
 	<form method="post">
