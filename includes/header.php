4672f0419e7a692347ba42b0a0712b8e9adf8cf5
Maxence <61044105+maxmlk@users.noreply.github.com>
2020-04-11 15:51:03 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="css/header.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <nav class="nav sticky-top bg-light">
     <p class="title">Livequestion</p>
     <p class="questions"><a href="les_questions.php">Les questions</a></p>
