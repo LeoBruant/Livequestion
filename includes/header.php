3dd7f5bb6706cb06469f8ccdd189bb0fe732cc4b
Léo Bruant <61012951+LeoBruant@users.noreply.github.com>
2020-04-26 16:11:00 +0200

    maj

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,18 @@
+<?php 
+    session_start();
+?>
 <link rel="stylesheet" href="css/header.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <nav class="nav sticky-top bg-light">
-    <p class="title">Livequestion</p>
+    <p class="titre">Livequestion</p>
     <p class="questions"><a href="les_questions.php">Les questions</a></p>
     <p class="ask_question"><a href="ask_question.php">Poser une question</a></p>
+    <?php
+        if(!empty($_SESSION['pseudo'])){
+            echo'<p class="bonjour">Bonjour '.$_SESSION['pseudo'].'</p><p><a href="index.php">deconnexion</a></p>
+            <p class="profil"><a href="profil.php">Profil</a></p>';
+        }
+        else{
+            echo'<p><a href="connexion.php">se connecter</a></p>';
+        }
+    ?>
 </nav>
