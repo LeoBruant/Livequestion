6926ebd2031ebc554520bfe99bbafa0b6f076dff
Léo Bruant <61012951+LeoBruant@users.noreply.github.com>
2020-06-19 20:40:33 +0200

    Add files via upload

---
+++ includes/pagination.php
@@ -0,0 +1,25 @@
+<?php
+    // page précédente
+
+    echo'<nav class="nav bg-light">
+        <p><a href="';
+        if($_GET['page'] > 1){
+            echo'les_questions.php?page='.($_GET['page'] - 1);
+        }
+        echo'" class="prev">«</a></p>';
+
+        // numéros de page
+
+        for ($i=0; $i < count($question)/30; $i++){ 
+            echo'<p class="page"><a href="les_questions.php?page='.($i+1).'">'.($i+1).'</a></p>';
+        }
+
+        // page suivante
+
+        echo'<p><a href="';
+        if($_GET['page'] < count($question)/30){
+            echo'les_questions.php?page='.($_GET['page'] + 1);
+        }
+        echo'" class="next">»</a></p>
+    </nav>';
+?>
