373b7e553e11dbc3aa5f7d71f9f0b559db21205c
Léo Bruant <61012951+LeoBruant@users.noreply.github.com>
2020-06-24 23:46:58 +0200

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
 <link rel="stylesheet" href="css/header.css">
 <nav class="nav sticky-top bg-light">
     <p class="titre">Livequestion</p>
-    <p class="questions"><a href="les_questions.php">Les questions</a></p>
+    <p class="questions"><a href="les_questions.php?page=1">Les questions</a></p>
     <p class="ask_question"><a href="ask_question.php">Poser une question</a></p>
     <?php
 
