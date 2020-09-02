f2ad5e9d87b8d989c24b67fa09b82b3e13435851
Leo Bruant <bruantleo@gmail.com>
2020-09-02 11:27:14 +0200

    maj

--- traitement/accepter.php
+++ traitement/accepter.php
@@ -7,14 +7,4 @@
     $query->execute();
 
     header('location: http://localhost/Livequestion-master/profil.php');
-=======
-<?php
-    session_start();
-    require_once('connexion_bdd.php');
-
-    $query = $connexion->prepare('UPDATE requete set Status_requete = "acceptee" where Id_envoyeur = '.$_GET['id'].' and Id_receveur = (SELECT Id_profil from profil where Pseudo_profil = "'.$_SESSION['pseudo'].'")');
-    $query->execute();
-
-    header('location: http://localhost/Livequestion-master/profil.php');
->>>>>>> 04471914325163b2d69142af37bd958f60748800
 ?>
